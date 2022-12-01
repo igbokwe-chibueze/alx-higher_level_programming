b77d275ea85a0a9cdcbab4726dc818cbcb92914a
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-11-30 16:04:53 -0800

    Task 6-correction01

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-for a in range(0, 8):
-
-        for b in range(a + 1, 10):
-
-                    print("{:d}{:d}".format(a, b), end=", ")
-
-                    print("{:d}{:d}".format(a + 1, b))
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
