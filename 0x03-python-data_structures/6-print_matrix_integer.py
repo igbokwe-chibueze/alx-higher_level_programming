6d2bfbb48b5bc8f354bfe1ea250133249a971949
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-12-06 13:37:14 -0800

    Task 6-correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
-""" prints a matrix of integers
-"""
 def print_matrix_integer(matrix=[[]]):
-    """ this function prints a matrix of integers"""
-    if matrix != [[]]:
-        for row in matrix:
-            for elem in row:
-                print("{:d}".format(elem), end=" "
-                      if elem != row[-1] else '\n')
-    else:
-        print()
+
+    for i in matrix:
+
+        print(' '.join("{:d}".format(j) for j in i))
