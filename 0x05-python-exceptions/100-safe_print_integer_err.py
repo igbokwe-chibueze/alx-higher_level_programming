d3d4900d06fbeb2f6186e8265136c8ca3799d789
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-12-21 11:59:39 -0800

    Task 7-correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,20 +2,11 @@
 
 from sys import stderr
 
-
-
-
-
 def safe_print_integer_err(value):
-
     try:
-
         print("{:d}".format(value))
 
     except (ValueError, TypeError) as te:
-
         stderr.write("Exception: {}\n".format(te))
-
         return (False)
-
     return (True)
