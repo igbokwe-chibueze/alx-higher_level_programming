575bdb4645f46c645d8059f7c6e614605d97387f
igbokwe-chibueze <igbokwechibueze@gmail.com>
2022-12-21 13:21:03 -0800

    Task 1 and 2-corrections

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 
-"""
-Square class definition.
-"""
+"""Square class definition."""
 
 class Square:
 
-    """
-    Square class with private instance attribute size.
-    """
+    """Square class with private instance attribute size."""
 
     def __init__(self, size=0):
 
-        """
+        """Initialize a new Square.
+
         Args:
             size: size of the square.
         """
-
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,19 +8,15 @@ class Square:
 
     def __init__(self, size=0):
 
-        """
+        """Initialize a new Square.
+
         Args:
             size: size of the square
         """
 
         if type(size) is not int:
-
             raise TypeError("size must be an integer")
-
         elif size < 0:
-
             raise ValueError("size must be >= 0")
-
         else:
-
             self.__size = size
