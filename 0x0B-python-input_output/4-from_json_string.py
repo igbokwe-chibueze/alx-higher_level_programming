3e61a8926b5916e45548c52fed1fe983df0c5e9d
Chibueze Igbokwe <igbokwechibuezeo@gmail.com>
2023-01-10 20:33:03 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
+"""returns python data structure"""
 import json
-"""
-This file contains a function that
-returns an object represented by a
-JSON string
-"""
 
 
 def from_json_string(my_str):
-    """
-    function to return object
-    represented by a JSON string
-    """
-    return (json.loads(my_str))
+    """returns an object(Python data structure)
+    represented by a json string"""
+    data = json.loads(my_str)
+    return data
