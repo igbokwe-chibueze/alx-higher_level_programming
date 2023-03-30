bf7e0ab56d2ce6340b5153468d0ff17490d14112
igbokwe-chibueze <igbokwechibueze@gmail.com>
2023-03-30 22:51:50 +0100

    Task 6
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,18 +5,21 @@ integers
 """
 
 
+def divide(array, low, high):
+    """divide and conquer"""
+
+    mid = int((high + low)/2)
+    if array[mid-1] <= array[mid] >= array[mid+1]:
+        return array[mid]
+    elif array[mid] > array[mid+1]:
+        return divide(array, low, mid-1)
+    elif array[mid] < array[mid+1]:
+        return divide(array, mid+1, high)
+
+
 def find_peak(list_of_integers):
-    """
-    function to find peak
-    """
-    l = len(list_of_integers)
-    if l == 0:
-        return
-    half = l // 2
-    if (half == l - 1 or list_of_integers[half] >=
-        list_of_integers[half + 1]) and (half == 0 or list_of_integers[half] >=
-                                         list_of_integers[half - 1]):
-        return (list_of_integers[half])
-    elif half != l - 1 and list_of_integers[half + 1] > list_of_integers[half]:
-        return (find_peak(list_of_integers[half + 1:]))
-    return (find_peak(list_of_integers[:half]))
+    """Find peak of a list"""
+
+    if not list_of_integers:
+        return None
+    return divide(list_of_integers, 0, len(list_of_integers)-1)
