936e819ff55dd31c3adf3cc8e2360847954bf0f2
igbokwe-chibueze <igbokwechibueze@gmail.com>
2023-03-30 22:41:13 +0100

    Correction1
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sending request to url and getting response
-curl -sL 0:5000/catch_me -X PUT "You got me!" -H "Origin: school" -d "user_id=98"
+curl -sL 0:5000/catch_me -X PUT "You got me!" -H "Origin: School" -d "user_id=98"
