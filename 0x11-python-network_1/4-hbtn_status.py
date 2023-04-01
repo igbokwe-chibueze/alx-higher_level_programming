ba5660e24fdcbfb38715bb2e239b4f9482d7806e
igbokwe-chibueze <igbokwechibueze@gmail.com>
2023-04-01 15:58:15 +0100

    Correction1
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ script that fetches url only with requests
 
 
 if __name__ == "__main__":
-    reply = requests.get('https://intranet.hbtn.io/status').text
+    reply = requests.get('https://alx-intranet.hbtn.io/status').text
     print("Body response:")
     print("\t- type: {}".format(type(reply)))
     print("\t- content: {}".format(reply))
